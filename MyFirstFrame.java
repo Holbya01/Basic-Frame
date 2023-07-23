6bfc28f2032cc750bef2164297522c394b17af97
@Holby_ <80401404+Holbya01@users.noreply.github.com>
2023-07-23 14:40:16 +0200

    Add files via upload

---
+++ MyFirstFrame.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut.ui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author holby1
+ */
+public class MyFirstFrame extends JFrame{
+    public MyFirstFrame(){
+        
+        setTitle("My first GUI");
+        setSize(200, 250);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.red);
+        setVisible(true);
+   }
+}
